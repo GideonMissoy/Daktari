50d815425ffff6e1248029046cde4a233625b718
GideonMissoy <giddykips2030@gmail.com>
2024-08-18 23:59:31 +0300

    Adding the Appointments' model.

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -20,3 +20,26 @@ class PatientProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name} - Patient"
+    
+
+class Appointment(models.Model):
+    APPOINTMENT_TYPES = [
+        ('Physical', 'Physical'),
+        ('Zoom', 'Zoom'),
+    ]
+
+    APPOINTMENT_STATUS = [
+        ('Scheduled', 'Scheduled'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled'),
+    ]
+
+    doctor = models.ForeignKey('DoctorProfile', on_delete=models.CASCADE)
+    patient = models.ForeignKey('PatientProfile', on_delete=models.CASCADE)
+    appointment_type = models.CharField(max_length=10, choices=APPOINTMENT_TYPES)
+    scheduled_time = models.DateTimeField()
+    status = models.CharField(max_length=10, choices=APPOINTMENT_STATUS, default='Scheduled')
+    zoom_link = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Appointment with Dr. {self.doctor.user.get_full_name} and {self.patient.user.get_full_name} on {self.scheduled_time}"

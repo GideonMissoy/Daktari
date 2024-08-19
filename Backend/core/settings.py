15e5d634653e07ea2e43af8093649af7f2470de7
mitsudata <solomonkaiaru154@gmail.com>
2024-08-19 19:00:32 +0300

    Events App, Backend/externals Commit

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -49,9 +49,11 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
+    'knox',
 
     'accounts',
     'api',
+    'events'
 ]
 
 MIDDLEWARE = [
--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/users/auth/', include('accounts.urls')),
-   # path("api/", include("api.urls")),
+    path('events/', include('events.urls', namespace='app_events')),
+    # path("api/", include("api.urls")),
 ]

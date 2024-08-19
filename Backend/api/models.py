634ac3005fdc3a4db161019af0058e87a3fc4d9c
GideonMissoy <giddykips2030@gmail.com>
2024-08-19 23:37:38 +0300

    Adding the Payments' model.

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -58,3 +58,16 @@ class MedicalRecord(models.Model):
         return f"Medical Record for {self.patient.user.get_full_name} by Dr. {self.doctor.user.get_full_name} on {self.date_created}"
 
 
+class Payment(models.Model):
+    patient = models.ForeignKey('PatientProfile', on_delete=models.CASCADE)
+    appointment = models.ForeignKey('Appointment', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_status = models.CharField(max_length=10, choices=[
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Failed', 'Failed')
+    ])
+    transaction_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Payment of {self.amount} by {self.patient.user.get_full_name} for appointment on {self.appointment.scheduled_time}"

031e2e88f1675bf065248cabeb05e8933b2de5cc
GideonMissoy <giddykips2030@gmail.com>
2024-08-19 23:35:19 +0300

    Adding patient medical record's model.

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -43,3 +43,18 @@ class Appointment(models.Model):
 
     def __str__(self):
         return f"Appointment with Dr. {self.doctor.user.get_full_name} and {self.patient.user.get_full_name} on {self.scheduled_time}"
+
+
+class MedicalRecord(models.Model):
+    patient = models.ForeignKey('PatientProfile', on_delete=models.CASCADE)
+    doctor = models.ForeignKey('DoctorProfile', on_delete=models.CASCADE)
+    appointment = models.ForeignKey('Appointment', on_delete=models.CASCADE)
+    notes = models.TextField()
+    prescriptions = models.TextField()  # You could also create a separate model if you want to structure this data further
+    diagnosis = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Medical Record for {self.patient.user.get_full_name} by Dr. {self.doctor.user.get_full_name} on {self.date_created}"
+
+
